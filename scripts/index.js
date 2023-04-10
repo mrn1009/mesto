4b69199668368ad8560fdd2ae688508a0b42b186
Marina Merzlyakova <marina.merzlyakova1009@yandex.ru>
2023-04-11 02:28:12 +0300

    реализовать js

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,119 @@
+const popup = document.querySelectorAll('.popup');
+const popupArray = Array.from(popup);
+const elementList = document.querySelector('.elements');
+const templateElement = document.querySelector('.template').content;
+const figurePopup = document.querySelector('.popup_type_figure');
+const figureContent = figurePopup.querySelector('.popup__figure');
+const figureImage = figureContent.querySelector('.popup__image');
+const figureCaption = figureContent.querySelector('.popup__caption');
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const popupContent = popup.querySelector('.popup__container')
-const popupCloseButton = popupContent.querySelector('.popup__close');
-const form = popupContent.querySelector('.popup__form')
-const nameInput = form.querySelector('.popup__input_type_name');
-const postInput = form.querySelector('.popup__input_type_post');
-const profileName = document.querySelector('.profile__name');   
-const profilePost = document.querySelector('.profile__post');     
+const addButton = document.querySelector('.profile__add-button');
+const editPopup = document.querySelector('.popup_type_edit');
+const addPopup = document.querySelector('.popup_type_add');
+const editForm = editPopup.querySelector('.popup__form_type_edit');
+const addForm = addPopup.querySelector('.popup__form_type_add');
+const nameField = editForm.querySelector('.popup__input_type_name');
+const postField = editForm.querySelector('.popup__input_type_post');
+const placeField = addForm.querySelector('.popup__input_type_place');
+const linkField = addForm.querySelector('.popup__input_type_link');
+const profileName = document.querySelector('.profile__name');
+const profilePost = document.querySelector('.profile__post');
 
-function openPopup() {
-    popup.classList.add("popup_opened");
-    nameInput.value = profileName.textContent;
-    postInput.value = profilePost.textContent;
-  }
- 
-function closePopup() {
-    popup.classList.remove("popup_opened");
-  }
-  
-function formSubmit(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profilePost.textContent = postInput.value;
-    closePopup();
-  }
 
-editButton.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", closePopup);
-form.addEventListener("submit", formSubmit);
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+} 
+
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+} 
+
+function createCard(item) {
+    const element = templateElement.querySelector('.element').cloneNode(true);
+    const image = element.querySelector('.element__image');
+    image.style.backgroundImage = `url('${item.link}')`;
+    element.querySelector('.element__title').innerText = item.name;
+
+    element.querySelector('.element__delete').addEventListener('click', function(event) {
+        element.remove();
+    })
+
+    const likeButton = element.querySelector('.element__like');
+    likeButton.addEventListener('click', function(event) {
+        likeButton.classList.toggle('element__like_active');
+    })
+
+    image.addEventListener('click', (event) => {
+        if (event.target.classList.contains('element__image')) {
+            openPopup(figurePopup);
+        }
+        figureImage.src = item.link;
+        figureImage.alt = item.name;
+        figureCaption.textContent = item.name;
+    })
+
+    return element;
+}
+
+function appendCard(item) {
+    const element = createCard(item);
+    elementList.prepend(element);
+}
+
+function submitEditForm(event) {
+    event.preventDefault()
+
+    profileName.textContent = nameField.value;
+    profilePost.textContent = postField.value;
+
+    closePopup(editPopup);
+}
+
+function addCard(event) {
+    event.preventDefault();
+
+    const place = placeField.value;
+    const link = linkField.value;
+    const item = {
+        name: place,
+        link: link
+    }
+    appendCard(item);
+    event.target.reset();
+
+    closePopup(addPopup);
+}
+
+function setUserInfoValues() {
+    nameField.value = profileName.textContent; 
+    postField.value = profilePost.textContent;
+} 
+
+function closeByOverlayClick(event) {
+    if (event.target.classList.contains('popup')) {
+        const openedPopup = document.querySelector('.popup_opened');
+        closePopup(openedPopup);
+    }
+} 
+initialCards.forEach(appendCard);
+
+editButton.addEventListener('click', ()=> {
+    openPopup(editPopup);
+    setUserInfoValues();
+})
+addButton.addEventListener('click', ()=> openPopup(addPopup));
+
+editForm.addEventListener('submit', submitEditForm);
+
+addForm.addEventListener('submit', addCard);
+
+popupArray.forEach(function(popup) {
+    const closeButton = popup.querySelector('.popup__close');
+    closeButton.addEventListener('click', ()=> {
+        closePopup(popup);
+    })
+}) 
+
+editPopup.addEventListener('click', closeByOverlayClick);
+addPopup.addEventListener('click', closeByOverlayClick);
+figurePopup.addEventListener('click', closeByOverlayClick);
--- scripts/initial-cards.js
+++ scripts/initial-cards.js
@@ -8,19 +8,19 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+      name: 'Карелия',
+      link: 'https://images.unsplash.com/photo-1602162991689-c97214fb18f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
     },
     {
       name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+      link: 'https://images.unsplash.com/photo-1557094005-176cbfe3554d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1634&q=80'
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+      name: 'Санкт-Петербург',
+      link: 'https://images.unsplash.com/photo-1555460285-763ba96917d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+      name: 'Москва',
+      link: 'https://images.unsplash.com/photo-1523509080324-9183f313dc50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHJ1c3NpYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
     }
   ];
